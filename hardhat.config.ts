ea9dc40f40891a4faa767f29a35d05eb329f4445
gauravchandrakar25 <gauravchandrakar25@gmail.com>
2023-12-08 22:54:28 +0530

    Hardhat configured as per mumbai testnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,26 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox-viem";
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
 
-const config: HardhatUserConfig = {
-  solidity: "0.8.19",
-};
-
-export default config;
+module.exports = {
+  defaultNetwork: "polygon_mumbai",
+  networks: {
+    hardhat: {
+    },
+    polygon_mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: ["787ea5cf0dba2d628e0cdb49dc59b7e22bcc5dca5d64555917396df4180e1c64"]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY
+  },
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+}
