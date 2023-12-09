26939ec41f8c87531c96fd720808f5a3c72169a9
gauravchandrakar25 <gauravchandrakar25@gmail.com>
2023-12-09 15:45:59 +0530

    Scroll added

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,23 +4,36 @@ require("@nomiclabs/hardhat-etherscan");
 module.exports = {
   defaultNetwork: "polygon_mumbai",
   networks: {
-    hardhat: {
-    },
+    hardhat: {},
     polygon_mumbai: {
       url: "https://rpc-mumbai.maticvigil.com",
-      accounts: ["787ea5cf0dba2d628e0cdb49dc59b7e22bcc5dca5d64555917396df4180e1c64"]
-    }
+      accounts: [
+        "787ea5cf0dba2d628e0cdb49dc59b7e22bcc5dca5d64555917396df4180e1c64",
+      ],
+    },
+    scrollSepolia: {
+      url: "https://sepolia-rpc.scroll.io/" || "",
+      accounts: [
+        "efa8f01063281248ad0fab29aa5a397195c1ba6fda92d423fc70bf602016ff50",
+      ],
+    },
+    scrollTestnet: {
+      url: "https://sepolia-rpc.scroll.io",
+      accounts: [
+        "efa8f01063281248ad0fab29aa5a397195c1ba6fda92d423fc70bf602016ff50",
+      ],
+    },
   },
   etherscan: {
-    apiKey: "6EJUM9X1Z6I8QME7E2696JTSB8RX1FA2MB"
+    apiKey: "6EJUM9X1Z6I8QME7E2696JTSB8RX1FA2MB",
   },
   solidity: {
     version: "0.8.20",
     settings: {
       optimizer: {
         enabled: true,
-        runs: 200
-      }
-    }
+        runs: 200,
+      },
+    },
   },
-}
+};
